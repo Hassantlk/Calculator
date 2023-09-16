c37508db5726a8a23b17e8359f0f7f9e83b99d2a
Hassan.TLK <hassan.tlk.85@gmail.com>
2023-09-16 10:35:37 +0330

    Complete Exeption.py but Still import not working

---
+++ exeption.py
@@ -0,0 +1,27 @@
+class InvalidFormatExeption(Exception):
+    def __init__(self, code, msg):
+        self.code = code
+        self.msg = msg
+    
+    
+    def __str__(self):
+        return f"{self.code} Number: {self.msg}"
+
+class InvalidNumberExeption(Exception):
+    def __init__(self, code, msg):
+        self.code = code
+        self.msg = msg
+    
+    
+    def __str__(self):
+        return f"{self.code} Number: {self.msg}"
+
+class InvalidOperatorExeption(Exception):
+    def __init__(self, code, msg):
+        self.code = code
+        self.msg = msg
+    
+    
+    def __str__(self):
+        return f"{self.code} Number: {self.msg}"
+
--- main.py
+++ main.py
@@ -4,6 +4,42 @@ import parse
 import logic.calc
 import calc
 
+# exeption
+class InvalidFormatExeption(Exception):
+    def __init__(self, code, msg):
+        self.code = code
+        self.msg = msg
+    
+    
+    def __str__(self):
+        return f"{self.code} Number: {self.msg}"
+
+class InvalidNumberExeption(Exception):
+    def __init__(self, code, msg):
+        self.code = code
+        self.msg = msg
+    
+    
+    def __str__(self):
+        return f"{self.code} Number: {self.msg}"
+
+class InvalidOperatorExeption(Exception):
+    def __init__(self, code, msg):
+        self.code = code
+        self.msg = msg
+    
+    
+    def __str__(self):
+        return f"{self.code} Number: {self.msg}"
+
+
+
+
+
+
+
+
+# parse
 def check_oprator(opr, a, b):
     dict_opr = {
         "+": add(a,b),
@@ -14,7 +50,7 @@ def check_oprator(opr, a, b):
 
     return dict_opr[opr]
 
-
+#calc
 def add(a, b):
     return a + b
 
@@ -27,10 +63,32 @@ def multipy(a, b):
 def divide(a, b):
     return a/b
 
+# main
+while quit:= input("Enter 3 with space [type 'quit or q' to Exit]: "):
+    try:
+        if quit.lower() == "quit" or quit.lower() == "q":
+            break
 
-a, opr, b = input("Enter 3 with space: ").split(" ")
-a = int(a)
-b = int(b)
+        if len(quit.split(" ")) != 3:
+            raise InvalidFormatExeption(1, "Please Enter right format [num1 operator num2] or enter 'q' to exit.")
 
-print(check_oprator(opr, a, b))
+        a, opr, b = quit.split(" ")
+        if opr not in ["+","-","*","/"]:
+            raise InvalidOperatorExeption(2, "between number enter right operator ['+','-','*','/'] or enter 'q' to exit.")
+
+        try:
+            a = int(a)
+            b = int(b)
+        except ValueError:
+            raise InvalidNumberExeption(3,"Enter number right")
+
+    except InvalidFormatExeption as ife:
+        print(ife.msg)
+    except InvalidNumberExeption as ine:
+        print(ine.msg)
+    except InvalidOperatorExeption as ioe:
+        print(ioe.msg)
+    else:
+        print(check_oprator(opr, a, b))
+
 
